b865e6f1e746adb28b468dc2dfca56f2c39704ca
Juliane Reis <julianereisb@gmail.com>
2020-08-02 04:32:26 -0300

    Componente de listagem de classes

--- src/app/components/classes-list/classes-list.component.ts
+++ src/app/components/classes-list/classes-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-classes-list',
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./classes-list.component.scss']
 })
 export class ClassesListComponent implements OnInit {
+  @Input() classes;
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.classes);
   }
 
 }
